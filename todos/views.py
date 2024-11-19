763e9d35f516011b0edd73f623be4e1df3707da4
marvynmesquita <marvynmesquita@gmail.com>
2024-11-19 19:39:31 -0300

    Criado Views e Paths

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import ListView, CreateView
+from models import Todo
 
 # Create your views here.
+class TodoListView(ListView):
+    model = Todo
+    template_name = "todo_list.html"
+
+class TodoCreateView(CreateView):
+    model = Todo
+    template_name = "todo_form.html"
+
